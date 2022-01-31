ef29b06caf936d7623de90f5c3075d0f42251a7e
Ilya <ilyaprikaz@gmail.com>
2022-01-31 21:01:49 +0400

    [feat] create findByCredentials

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -37,15 +38,33 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-/** Middleware */
+/** 
+ * Middleware 
+ */
+// Login user
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login.");
+  }
+
+  return user
+};
+
+// Hash the plain text password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
 
-  if (user.isModified('password')) {
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
-  next()
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
